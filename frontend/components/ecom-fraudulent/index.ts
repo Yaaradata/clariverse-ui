4718a3ea096fdba2bcd2c556e65e09f1d364100f
nitishks-27 <common.tech@yaaralabs.ai>
2025-12-09 01:46:45 +0530

    update

--- frontend/components/ecom-fraudulent/index.ts
+++ frontend/components/ecom-fraudulent/index.ts
@@ -1,10 +1,8 @@
 // Main dashboard components
 export { default as FraudRiskSnapshot } from './FraudRiskSnapshot';
 export { default as AIPatternBrain } from './AIPatternBrain';
-export { default as ThreatIntelligenceGrid } from './ThreatIntelligenceGrid';
 export { default as ForensicEvidenceModal } from './ForensicEvidenceModal';
 export { default as CrossChannelFraudConsistencyWheel } from './CrossChannelFraudConsistencyWheel';
-export { default as FraudFailurePointMap } from './FraudFailurePointMap';
 export { default as RegionalFraudBurdenBar } from './RegionalFraudBurdenBar';
 
 // Export types
